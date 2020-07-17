d562f4821fd9207ef31384c5c072a8f1c401a1b0
ArcanumOvO <49122229+ArcanumOvO@users.noreply.github.com>
2020-07-17 16:14:12 +0500

    init2

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="./css/style.css">
     <script src="./js/jquery-3.5.1.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src=""></script>
     <script src="./js/script.js"></script>
     <title>Тестовое задание для iq-online</title>
 </head>
